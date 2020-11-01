5b8782618f3a749da36a7ee50552cebc7d955d93
Pablo Andrés Dorado Suárez <hola@pablodorado.com>
2020-10-31 20:37:27 -0500

    Initial commit: App skeleton

---
+++ poi.config.js
@@ -0,0 +1,23 @@
+const path = require('path')
+const pkg = require('./package')
+
+module.exports = {
+  entry: [
+    'src/polyfills.js',
+    'src/index.js'
+  ],
+  html: {
+    title: pkg.productName,
+    description: pkg.description,
+    environment: process.env.NODE_ENV ?? 'development',
+    template: path.join(__dirname, 'index.ejs')
+  },
+  postcss: {
+    plugins: [
+      // Your postcss plugins
+    ]
+  },
+  presets: [
+    require('poi-preset-bundle-report')()
+  ]
+}
---
+++ src/components/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <card style="default" :info="user"></card>
+</template>
+
+<script>
+import Card from './Card.vue';
+
+export default {
+  name: 'app',
+  components: {
+    Card
+  },
+  data() {
+    return {
+      user: {
+        name: 'Pablo Andrés Dorado Suárez',
+        pictureThumbnail: 'https://starrgate.s3.amazonaws.com:443/CACHE/images/users/9452d46ba9f94fe9851ab4d0bbcb33c81b390f7a/profile_oeuoVH2/2387b40c6bdbd107da26d37a285018f6.jpg'
+      }
+    };
+  }
+}
+</script>
+
+<!-- CSS libraries -->
+<style src="normalize.css/normalize.css"></style>
+
+<!-- Global CSS -->
+<style>
+body {
+  background: transparent;
+  font-family: Muli, serif;
+}
+</style>
+
+<style scoped>
+</style>
