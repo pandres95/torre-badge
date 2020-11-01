f1e7b4d3a10b7554e1a7ff50cf7ccfcad3d3a322
Pablo Andrés Dorado Suárez <hola@pablodorado.com>
2020-10-31 23:39:26 -0500

    fix: Invalid fetch API on Node

--- api/[username].js
+++ api/[username].js
@@ -1,8 +1,10 @@
 'use strict';
 
+const fetch = require('node-fetch');
+
 module.exports = async (request, response) => {
   const { query: { username } } = request;
 
-  const result = await fetch(`https://torre.bio/api/bios/${search.get('username')}`);
+  const result = await fetch(`https://torre.bio/api/bios/${username}`);
   return response.send(await result.json());
 }
