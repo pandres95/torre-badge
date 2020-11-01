605e864c9a96d7dce391815db6c164035e70aaf6
Pablo Andrés Dorado Suárez <hola@pablodorado.com>
2020-10-31 23:04:23 -0500

    change: Max-width / add: fetchData

--- src/components/App.vue
+++ src/components/App.vue
@@ -1,5 +1,7 @@
 <template>
-  <card style="default" :info="user"></card>
+  <div class="wrapper">
+    <card style="default" :info="user"></card>
+  </div>
 </template>
 
 <script>
@@ -17,6 +19,16 @@ export default {
         pictureThumbnail: 'https://starrgate.s3.amazonaws.com:443/CACHE/images/users/9452d46ba9f94fe9851ab4d0bbcb33c81b390f7a/profile_oeuoVH2/2387b40c6bdbd107da26d37a285018f6.jpg'
       }
     };
+  },
+  async beforeMount() {
+    await this.fetchData();
+  },
+  methods: {
+    async fetchData() {
+      const search = new URLSearchParams(window.location.search);
+      const response = await fetch(`https://torre.bio/api/bios/${search.get('username')}`);
+      this.user = response.json();
+    }
   }
 }
 </script>
@@ -26,9 +38,27 @@ export default {
 
 <!-- Global CSS -->
 <style>
+html {
+  width: 100%;
+  overflow: hidden;
+  box-sizing: border-box;
+}
+
 body {
+  box-sizing: border-box;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  max-width: 100vh;
   background: transparent;
   font-family: Muli, serif;
+}
+
+.wrapper {
+  width: 100%;
+  max-width: 720px;
+
+  padding: 1em;
 }
 </style>
 
